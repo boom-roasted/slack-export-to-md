ae566981278fdea61fbd89ca62f6c6e45c87eebd
boom-roasted <noah.prezant@gmail.com>
2021-01-27 23:21:56 -0700

    Add user profile information reader

--- process.py
+++ process.py
@@ -1,7 +1,7 @@
 from __future__ import annotations # For return types
 
 from pathlib import Path
-from typing import List, Optional, OrderedDict
+from typing import Dict, List, Optional, OrderedDict
 from collections import OrderedDict
 from datetime import datetime
 import json
@@ -76,7 +76,7 @@ class Message:
                     continue
 
             # Get message attributes
-            text, user, ts = (item.get(attr, ) for attr in required_attrs)
+            text, user, ts = (item.get(attr) for attr in required_attrs)
             
             # Get optional attributes. In this case, just the thread time stamp
             thread_ts = item.get("thread_ts", None)
@@ -106,13 +106,58 @@ class Thread(Message):
             for reply in self.replies:
                 f.write(reply.to_markdown_s() + "\n\n")
 
-
     @staticmethod
     def create(m: Message) -> Thread:
         """Initialize a thread with a head message"""
         if m.thread_ts is None:
             raise AttributeError(f"Message must have 'thread_ts' to be initialized to a thread. Message: {m}")
         return Thread(m.text, m.user, m.ts, m.thread_ts)
+
+
+class User:
+    def __init__(self, id: str, name: str, real_name: str, real_name_normalized: str) -> None:
+        self.id = id
+        self.name = name
+        self.real_name = real_name
+        self.real_name_normalized = real_name_normalized
+        self.initials = "".join([part[0] for part in real_name_normalized.split(" ")])
+
+    @staticmethod
+    def create(user: dict) -> User:
+        """Create a User from the dict containing a single user's information in a slack export"""
+        attrs = user.keys()
+        required_attrs = ["id", "name", "profile"]
+
+        for attr in required_attrs:
+            if not attr in attrs:
+                raise AttributeError(f"User definition does not contain necessary attribute: {attr}. Definition: {user}")
+
+        id, name, profile = (user.get(attr) for attr in required_attrs)
+
+        if not isinstance(profile, dict):
+            raise AttributeError(f"Expected user profile to be a dict, not {type(profile)}.")
+
+        required_profile_attrs = ["real_name", "real_name_normalized"]
+
+        for attr in required_profile_attrs:
+            if not attr in profile.keys():
+                raise AttributeError(f"User profile must contain a '{attr}' field. Got profile: {profile}")
+
+        real_name, real_name_normalized = (profile.get(attr) for attr in required_profile_attrs)
+
+        return User(str(id), str(name), str(real_name), str(real_name_normalized))
+
+    @staticmethod
+    def create_map(filename: Path) -> Dict[str, User]:
+        users: Dict[str, User] = {} # user id, user
+        with open(filename, "r") as f:
+            data = json.load(f)
+
+        for user_data in data:
+            user = User.create(user_data)
+            users[user.id] = user
+
+        return users
 
 
 class Channel:
@@ -149,17 +194,20 @@ class Channel:
         return list(threads.values())
 
 
-
-
 if __name__ == "__main__":
 
-    folder = Path.cwd() / "export/help"
+    # Read user information
+    # TODO combine with export markdown
+    user_file = Path.cwd() / "export/users.json"
+    users = User.create_map(user_file)
 
-    channel = Channel.create(folder)
-
+    # Read channel information
+    channel_dir = Path.cwd() / "export/help"
+    channel = Channel.create(channel_dir)
     print(f"Found {len(channel.messages)} messages with {len(channel.threads)} discrete threads")
 
-    outdir = Path.cwd() / "md" # for 'markdown'
+    # Write out markdown formatted thread conversations
+    outdir = Path.cwd() / "md"
     outdir.mkdir(exist_ok=True)
 
     for thread in channel.threads:
